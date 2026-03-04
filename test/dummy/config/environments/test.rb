a30b199aeebd2ff50328ab7c90f7cff640c2f516
Kaka Ruto <kr@kakaruto.com>
2026-03-04 06:21:25 +0300

    Prepare 0.2.0 release workflow and CI

--- test/dummy/config/environments/test.rb
+++ test/dummy/config/environments/test.rb
@@ -5,5 +5,5 @@ Rails.application.configure do
   config.consider_all_requests_local = true
   config.action_controller.perform_caching = false
   config.active_support.deprecation = :stderr
-  config.active_record.maintain_test_schema = true
+  config.active_record.maintain_test_schema = false
 end
